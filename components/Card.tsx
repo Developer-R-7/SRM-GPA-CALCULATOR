116cb864036ee4ee158c9538385ae5cd91cf4813
Rushi Patel <rushinasa06@gmail.com>
2022-12-10 13:24:57 +0530

    :zap: improve frontend performance

---
+++ components/Card.tsx
@@ -0,0 +1,11 @@
+export default function Card() {
+  return (
+    <div className="flex flex-col justify-center items-center">
+      <div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
+        <div className="flex flex-col items-center pb-10">
+          <h1 className="p-11">Helloworld</h1>
+        </div>
+      </div>
+    </div>
+  );
+}
