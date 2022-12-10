35ea6e2ed7b4d37baa7d9e7a91b224bf397115ea
Rushi Patel <rushinasa06@gmail.com>
2022-12-10 18:32:57 +0530

    :art: fix search bar styling

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -6,10 +6,10 @@ export default function HeroSection() {
     <section className="bg-white dark:bg-gray-900">
       <div className="grid max-w-screen-xl h-screen px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
         <div className="mr-auto place-self-center lg:col-span-7">
-          <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">
+          <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl p-6 text-center xl:text-6xl dark:text-white">
             Payments tool for software companies
           </h1>
-          <p className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
+          <p className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg  text-center lg:text-xl dark:text-gray-400">
             From checkout to global sales tax compliance, companies around the
             world use Flowbite to simplify their payment stack.
           </p>
